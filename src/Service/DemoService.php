7fe64815cc444fa7e9944ed0d2a915a19091a890
Moritz Petzka <moritz@petzka.com>
2020-06-08 18:47:20 +0200

    module widget and frontend component

--- src/Service/DemoService.php
+++ src/Service/DemoService.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of [petzka/demo-bundle].
+ *
+ * (c) Moritz Petzka
+ *
+ * @license LGPL-3.0-or-later
+ */
+
 namespace Petzka\DemoBundle\Service;
 
 class DemoService
